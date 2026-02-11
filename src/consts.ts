0eb940e39a8392cf999582a8412669ba903d6a61
Himanshu1747 <himanshusaxena1747@gmail.com>
2026-02-11 14:18:32 +0530

    Fix: Added gitignore and removed junk

--- src/consts.ts
+++
@@ -1,9 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Campbell for Congress | Empower Change Today';
-export const SITE_DESCRIPTION = 'Discover Shelby Campbell\'s grassroots campaign advocating for working families, community connection, and political reform in Detroit\'s District.';
-
-
-
-
